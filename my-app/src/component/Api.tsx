e9f898b5fe5f1349c97a08a6dce91f1695c90659
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-02-24 16:31:59 +0100

    changed layout in Main, moved API to the right file

--- my-app/src/component/Api.tsx
+++ my-app/src/component/Api.tsx
@@ -1,60 +1,70 @@
 import React, {useState} from "react";
 
-interface State {
-    main: string
-    name: string
-    weather: string
+    interface WeatherData {
 
-}
+        base: string
+        clouds: { all: number }
+        cod: number
+        coord: { lon: number, lat: number }
+        dt: number
+        id: number
+        main: { temp: number, feels_like: number, temp_min: number, temp_max: number, pressure: number, humidity: number }
+        name: string
+        sys: { type: number, id: number, country: string, sunrise: number, sunset: number }
+        timezone: number
+        visibility: number
+        weather: [{ id: number, main: string, description: string, icon: string }]
+        wind: { speed: number, deg: number }
 
-
-function Api() {
-    //Väder API
-
-    const APIKey = "661acb0f32a6cfbc4b06bb0c529a83aa";
-    const [weatherData, setWeatherData] = useState([{}])
-    const [city, setCity] = useState("")
-
-    const getWeather = (event: { key: string; }) => {
-        if (event.key === "Enter") {
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${APIKey}`)
-                .then(
-                    response => response.json()
-                ).then(
-                data => {
-                    let api = data.main;
-                    setWeatherData(data)
-                    setCity("")
-                    return api;
-                }
-            )
-        }
     }
-    return (
-        <div className="App">
-            <p>Välkommen till Weather app! Skriv en stad för att se vädret!</p>
-            <input
-                className="input"
-                placeholder="Skriv en stad..."
-                onChange={e => setCity(e.target.value)}
-                value={city}
-                onKeyPress={getWeather}
-            />
-            {typeof weatherData.main ==='undefined' ?(
-                <div>
 
-                </div>
-            ): (
-                <div>
-                    <h2>Stad: {weatherData.name}</h2>
-                    {console.log(weatherData)}
-                    <p>Temperatur: {Math.round(weatherData.main.temp)} grader</p>
-                    <p>Väder: {weatherData.weather[0].main}</p>
-                </div>
-            )}
+    function Api() {
+        //Väder API
+        const APIKey = "661acb0f32a6cfbc4b06bb0c529a83aa";
+        const language = "sv";
+        const [weatherData, setWeatherData] = useState<WeatherData[]>([])
+        const [city, setCity] = useState("")
 
-        </div>
-    );
+        const getWeather = (event: { key: string; }) => {
+            if (event.key === "Enter") {
+                fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${APIKey}&lang=se`)
+                    .then(
+                        response => response.json()
+                    ).then(
+                    data => {
+                        setWeatherData(data)
+                        setCity("")
+                    }
+                )
+            }
+        }
+        return (
+            <div className="Api">
+
+                {/* <Weather /> */}
+                <input
+                    className="input"
+                    placeholder="Skriv en stad..."
+                    onChange={e => setCity(e.target.value)}
+                    value={city}
+                    onKeyPress={getWeather}
+                />
+                {typeof weatherData.main === 'undefined' ? (
+                    <div>
+                        <p>Välkommen till Weather app! Skriv en stad för att se vädret!</p>
+                    </div>
+                ) : (
+                    <div>
+                        <p>Stad: {weatherData.name}</p>
+                        <p>{Math.round(weatherData.main.temp)} grader</p>
+                        <p>{weatherData.weather[0].description}</p>
+                    </div>
+                )}
+
+            </div>
+        );
+
 }
-
 export default Api;
+
+
